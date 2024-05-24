ebddb444e44f02676aca03d1e9109f60432048c8
hycinth <realhycinth@gmail.com>
2024-05-25 04:40:36 +0800

    init.sh

---
+++ init.sh
@@ -0,0 +1,6 @@
+export TOOLCHAIN_NAME="stage2"
+# export TOOLCHAIN_NAME="nightly"
+export RUSTC_LOG="rustc_meata=debug,solcon_instrumenter=debug"
+export RUSTC_WRAPPER="/home/hycinth/src/solcon_instrumenter/target/debug/solcon_instrumenter"
+export RUST_SYSROOT=$(rustc +$TOOLCHAIN_NAME --print=sysroot)
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$RUST_SYSROOT/lib/"
