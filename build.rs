ebab96edd05fa94d2e80821d13d6720bb4deb7e0
hycinth <realhycinth@gmail.com>
2024-05-27 16:26:31 +0800

    uise build.rs to set RUST_SYSROOT from rustc

---
+++ build.rs
@@ -0,0 +1,17 @@
+fn get_sysroot_from_rustc() -> Option<String> {
+    let out = std::process::Command::new("rustc").arg("--print=sysroot")
+    .current_dir(".").output();
+    if let Ok(out) = out {
+        if out.status.success() {
+            let sysroot = std::str::from_utf8(&out.stdout).unwrap().trim();
+            return Some(sysroot.to_owned());
+        }
+    }
+    None
+}
+
+fn main() {
+    if let Some(sysroot) = get_sysroot_from_rustc() {
+        println!("cargo::rustc-env=RUST_SYSROOT={}", sysroot);
+    }
+}
