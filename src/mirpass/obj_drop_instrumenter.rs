71baa8009d2dfcd0e40dd44e0645a6ea64f2cccb
hycinth <realhycinth@gmail.com>
2024-06-05 00:51:31 +0800

    implement instrument_drop_after

--- src/mirpass/obj_drop_instrumenter.rs
+++ src/mirpass/obj_drop_instrumenter.rs
@@ -78,7 +78,7 @@ pub trait ObjectDropInstrumenter {
         body: &Body<'tcx>, monitors: &MonitorsInfo,
         drop_at_block: BasicBlock,
     ) -> Option<(MirPatch<'tcx>, BasicBlock )> {
-        let Some(our_func_def_id) = self.before_monitor_def_id(monitors) else { warn!("monitors.mutexguard_drop_before_fn.is_none"); return None; };
+        let Some(our_func_def_id) = self.before_monitor_def_id(monitors) else { return None; };
         let drop_at_block_data = &body.basic_blocks[drop_at_block];
         let terminator = drop_at_block_data.terminator();
             match &terminator.kind {
@@ -137,28 +137,28 @@ pub trait ObjectDropInstrumenter {
                         Operand::Copy(place) | Operand::Move(place) => place,
                         Operand::Constant(..) => panic!("running drop on constant")
                     };
-                    let temp_ref_to_droping_obj = Place::from(patch.new_temp(Ty::new_imm_ref(tcx, tcx.lifetimes.re_erased, arg_ty), DUMMY_SP));
+                    let temp_ref_to_droping_obj = Place::from(patch.new_temp(Ty::new_imm_ref(tcx, tcx.lifetimes.re_erased, arg_ty), *fn_span));
                     patch.add_assign(patch.terminator_loc(body, drop_at_block), temp_ref_to_droping_obj, Rvalue::Ref(
                         tcx.lifetimes.re_erased,
                         BorrowKind::Shared,
                         place_droping_obj,
                     ));
                     let our_call_args = vec![
-                        build_drop_callsite_str_operand(tcx, fn_span.clone()),
+                        build_drop_callsite_str_operand(tcx, *fn_span),
                         Spanned {
                             node: Operand::Move(temp_ref_to_droping_obj),
-                            span: DUMMY_SP,
+                            span: *fn_span,
                         }
                     ];
-                    let temp_ret = patch.new_temp(tcx.types.unit, DUMMY_SP);
+                    let temp_ret = patch.new_temp(tcx.types.unit, *fn_span);
                     patch.patch_terminator(drop_at_block, TerminatorKind::Call{
-                        func: crate::utils::instantiate_our_func(tcx, our_func_def_id, generic_args, DUMMY_SP),
+                        func: crate::utils::instantiate_our_func(tcx, our_func_def_id, generic_args, *fn_span),
                         args: our_call_args,
                         destination: Place::from(temp_ret),
                         target: Some(new_bb_run_drop),
                         unwind: unwind.clone(),
                         call_source: CallSource::Misc,
-                        fn_span: DUMMY_SP,
+                        fn_span: *fn_span,
                     });
                     Some((patch, new_bb_run_drop))
                 }
@@ -171,6 +171,110 @@ pub trait ObjectDropInstrumenter {
         body: &Body<'tcx>, monitors: &MonitorsInfo,
         drop_at_block: BasicBlock
     ) -> Option<(MirPatch<'tcx>, BasicBlock )> {
-        None
+        let Some(our_func_def_id) = self.after_monitor_def_id(monitors) else { return None; };
+        let drop_at_block_data = &body.basic_blocks[drop_at_block];
+        let terminator = drop_at_block_data.terminator();
+        match &terminator.kind {
+            TerminatorKind::Drop { place, target, unwind, replace} => {
+                let ty = place.ty(&body.local_decls, tcx).ty;
+                let TyKind::Adt(adt_def, generic_args) = ty.kind() else {
+                    unreachable!();
+                };
+                let mut patch = MirPatch::new(body);
+                let temp_ref_to_droping_obj = Place::from(patch.new_temp(Ty::new_imm_ref(tcx, tcx.lifetimes.re_erased, ty), DUMMY_SP));
+                patch.add_assign(patch.terminator_loc(body, drop_at_block), temp_ref_to_droping_obj, Rvalue::Ref(
+                    tcx.lifetimes.re_erased,
+                    BorrowKind::Shared,
+                    place.clone(),
+                ));
+
+                let our_call_args = vec![
+                    build_drop_callsite_str_operand(tcx, build_drop_span(tcx, &body, drop_at_block)),
+                    Spanned {
+                        node: Operand::Move(temp_ref_to_droping_obj),
+                        span: DUMMY_SP,
+                    }
+                ];
+
+                let temp_ret = patch.new_temp(tcx.types.unit, DUMMY_SP);
+                let new_bb_run_call = patch.new_block(BasicBlockData {
+                    statements: vec![],
+                    terminator: Some(Terminator {
+                        kind: TerminatorKind::Call { 
+                            func: crate::utils::instantiate_our_func(tcx, our_func_def_id, generic_args, DUMMY_SP),
+                            args: our_call_args, 
+                            destination: temp_ret.into(), 
+                            target: Some(target.clone()),
+                            unwind: unwind.clone(), 
+                            call_source: CallSource::Misc, 
+                            fn_span: DUMMY_SP },
+                        source_info: terminator.source_info.clone(),
+                    }),
+                    is_cleanup: false,
+                });
+                patch.patch_terminator(drop_at_block, TerminatorKind::Drop{
+                    place: *place,
+                    target: new_bb_run_call,
+                    unwind: unwind.clone(),
+                    replace: replace.clone(),
+                });
+                return Some((patch, drop_at_block));
+            },
+            TerminatorKind::Call{ func, args, destination, target, unwind, call_source, fn_span} => {
+                let Some(generic_args) = utils::get_function_generic_args(tcx, &body.local_decls, &func) else {
+                    warn!("Found call to std/core::mem::drop but fail to get function generic_args");
+                    unreachable!()
+                };
+                let arg_ty = generic_args.type_at(0);
+                let TyKind::Adt(adt_def, generic_args) = arg_ty.kind() else {
+                    unreachable!()
+                };
+                let mut patch = MirPatch::new(body);
+                let place_droping_obj = match args[0].node {
+                    Operand::Copy(place) | Operand::Move(place) => place,
+                    Operand::Constant(..) => panic!("running drop on constant")
+                };
+                let temp_ref_to_droping_obj = Place::from(patch.new_temp(Ty::new_imm_ref(tcx, tcx.lifetimes.re_erased, arg_ty), *fn_span));
+                patch.add_assign(patch.terminator_loc(body, drop_at_block), temp_ref_to_droping_obj, Rvalue::Ref(
+                    tcx.lifetimes.re_erased,
+                    BorrowKind::Shared,
+                    place_droping_obj,
+                ));
+                let our_call_args = vec![
+                    build_drop_callsite_str_operand(tcx, *fn_span),
+                    Spanned {
+                        node: Operand::Move(temp_ref_to_droping_obj),
+                        span: *fn_span,
+                    }
+                ];
+                let temp_ret = patch.new_temp(tcx.types.unit, *fn_span);
+                let new_bb_run_call = patch.new_block(BasicBlockData {
+                    statements: vec![],
+                    terminator: Some(Terminator {
+                        kind: TerminatorKind::Call { 
+                            func: crate::utils::instantiate_our_func(tcx, our_func_def_id, generic_args, *fn_span),
+                            args: our_call_args, 
+                            destination: temp_ret.into(), 
+                            target: target.clone(),
+                            unwind: unwind.clone(), 
+                            call_source: call_source.clone(), 
+                            fn_span: fn_span.clone() },
+                        source_info: terminator.source_info.clone(),
+                    }),
+                    is_cleanup: false,
+                });
+                patch.patch_terminator(drop_at_block, TerminatorKind::Call{
+                    func: func.clone(),
+                    args: args.clone(),
+                    destination: destination.clone(),
+                    target: Some(new_bb_run_call),
+                    unwind: unwind.clone(),
+                    call_source: call_source.clone(),
+                    fn_span: fn_span.clone(),
+                });
+                return Some((patch, drop_at_block));
+            },
+            _  => unreachable!(),
+        }
     }
 }
