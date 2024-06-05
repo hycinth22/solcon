e72c4f4e92839c8376de09a2fdbfd96875709859
hycinth <realhycinth@gmail.com>
2024-06-05 22:59:10 +0800

    update testcase

--- testcases/testcase_std_sync/src/main.rs
+++ testcases/testcase_std_sync/src/main.rs
@@ -4,7 +4,20 @@ use std::sync::Barrier;
 use std::sync::Condvar;
 use std::sync::Arc;
 use std::thread;
+use std::time::Duration;
 
+fn spwan_notify_thread(cm_pair2: Arc<(Condvar, Mutex<bool>)> ) {
+    thread::spawn(move || {
+        let (condvar, mutex) = &*cm_pair2;
+        let mut guard = mutex.lock().unwrap();
+        *guard = true;
+        drop(guard);
+        condvar.notify_one();
+    });
+}
+
+
+#[allow(deprecated)]
 fn main() {
     let mutex = Mutex::new(111);
     let guard = mutex.lock().unwrap();
@@ -20,16 +33,34 @@ fn main() {
     drop(guard);
     let guard = rwlock.try_write().unwrap();
     drop(guard);
+
     let barrier = Barrier::new(1);
     barrier.wait();
-    let cm_pair = Arc::new((Condvar::new(), Mutex::new(())));
-    let cm_pair2 = Arc::clone(&cm_pair);
+
+    testconvar();
+}
+
+fn testconvar() {
+
+    let cm_pair = Arc::new((Condvar::new(), Mutex::new(false)));
+
     let (condvar, mutex) = &*cm_pair;
-    let gurad = mutex.lock().unwrap();
-    thread::spawn(move || {
-        let (condvar, mutex) = &*cm_pair2;
-        drop(mutex.lock());
-        condvar.notify_one();
-    });
-    let guard = condvar.wait(gurad);
+    let guard = mutex.lock().unwrap();
+
+    spwan_notify_thread(Arc::clone(&cm_pair));
+    let guard = condvar.wait(guard).unwrap();
+
+    spwan_notify_thread(Arc::clone(&cm_pair));
+    let (guard, _timeoutresult) = condvar.wait_timeout(guard, Duration::MAX).unwrap();
+
+    spwan_notify_thread(Arc::clone(&cm_pair));
+    let (mut guard, _timeoutresult) = condvar.wait_timeout_ms(guard, u32::MAX).unwrap();
+
+    *guard = false;
+    spwan_notify_thread(Arc::clone(&cm_pair));
+    let mut guard = condvar.wait_while(guard, |val| *val).unwrap();
+
+    *guard = false;
+    spwan_notify_thread(Arc::clone(&cm_pair));
+    let guard = condvar.wait_timeout_while(guard, Duration::MAX, |val| *val).unwrap();
 }
